c44d9042c7050d920a4cfdedf38631dc510af71f
Jamie Everett <jreverett2442@gmail.com>
2021-03-16 21:16:47 +0000

    More thumbnail search term changes

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ client.on('message', (message) => {
           }
 
           const imageSearch = new Promise((resolve, reject) => {
-            gis(`${longName} logo inurl:.png`, (error, results) => {
+            gis(`${longName} logo +.png -.svg`, (error, results) => {
               if (error) {
                 reject(error);
               } else {
